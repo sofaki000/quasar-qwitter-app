4b5ed2e97404c61357781a723d7d61b46500bc0b
skardami <s.kardami@b-open.com>
2021-03-14 16:56:29 +0200

    hot fixes

--- src/boot/firebase.js
+++ src/boot/firebase.js
@@ -3,12 +3,12 @@ import firebase from "firebase/app"
 
 import "firebase/firestore"
 const firebaseConfig = {
-  apiKey: "AIzaSyCA_mf7VdIdAhH4lrxcTLFFcHzhXinKgIo",
-  authDomain: "qwitter-6dfe2.firebaseapp.com",
-  projectId: "qwitter-6dfe2",
-  storageBucket: "qwitter-6dfe2.appspot.com",
-  messagingSenderId: "769439257458",
-  appId: "1:769439257458:web:c0fbec8242341c183083eb"
+  apiKey: ****enter api key*******,
+  authDomain: ***************,
+  projectId: ***************,,
+  storageBucket: ***************,,
+  messagingSenderId: ***************,,
+  appId: ***************,
 };
 
 // Initialize Firebase
